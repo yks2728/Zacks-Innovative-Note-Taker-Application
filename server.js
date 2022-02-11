e2e7742b522359ac51ff72930ff7bef416d86a7a
Zack Greenfield <zackg007@verizon.net>
2022-02-11 18:03:32 -0500

    reformatted document

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const {data} = require('./db/db.json');
-
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const { data } = require("./db/db.json");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
-app.get('/api/notes', (req, res) => {
-    res.json(data);
+app.get("/api/notes", (req, res) => {
+  res.json(data);
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
+  console.log(`API server now on port ${PORT}`);
 });
