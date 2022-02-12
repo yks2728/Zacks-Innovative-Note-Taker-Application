cb9ba8ce75940355615358679011bdbdb4c2c6c0
Zack Greenfield <zackg007@verizon.net>
2022-02-12 11:13:06 -0500

    moved html routes to server.js page

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,14 +1,14 @@
-const path = require('path');
+// const path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../../public/index.html'));
+// });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
-});
+// app.get('/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../../public/notes.html'));
+// });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../../public/index.html'));
+// });
 
--- server.js
+++ server.js
@@ -16,8 +16,21 @@ app.use(express.json());
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 
-app.get('/api/notes', (req, res) => {
-  res.json('notes');
+// app.get('/notes', (req, res) => {
+//   res.json('notes');
+// });
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
