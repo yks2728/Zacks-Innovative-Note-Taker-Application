f5e0c19c40b37743c9897698e525e59d9b5790f8
Zack Greenfield <zackg007@verizon.net>
2022-02-12 10:26:19 -0500

    changed route structuring

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
-const { notes } = require("./db/db.json");
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { notes } = require('./db/db.json');
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const apiRoutes = require("./routes/apiRoutes");
-// const htmlRoutes = require("./routes/htmlRoutes");
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use("/api", apiRoutes);
-// app.use("/", htmlRoutes);
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
 
 app.get('/api/notes', (req, res) => {
   res.json('notes');
